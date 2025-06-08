ed9008f46a84598bd722ef694e26b855b275ba4f
Akhil AR <akhilnta1990@gmail.com>
2025-06-08 12:03:32 +0530

    artifacts add

--- src/exception.py
+++ src/exception.py
@@ -1,17 +1,16 @@
 import sys
 from src.logger import logging
-<<<<<<< HEAD
-=======
-
->>>>>>> cfa5f285d6d3b1352380e241d68ec5964121652c
 
 
-def error_message_detail(error,error_detail:sys):
-    _,_,exc_tb=error_detail.exc_info()
-    file_name=exc_tb.tb_frame.f_code.co_filename
-    error_message="Error occured in python script name [{0}] line number [{1}] error message[{2}]".format(
-     file_name,exc_tb.tb_lineno,str(error))
 
+def error_message_detail(error, error_detail: sys):
+    _, _, exc_tb = error_detail.exc_info()
+    if exc_tb is not None:
+        file_name = exc_tb.tb_frame.f_code.co_filename
+        error_message = "Error occured in python script name [{0}] line number [{1}] error message[{2}]".format(
+            file_name, exc_tb.tb_lineno, str(error))
+    else:
+        error_message = str(error)
     return error_message
 
 class CustomException(Exception):
@@ -21,14 +20,9 @@ class CustomException(Exception):
     
     def __str__(self):
         return self.error_message
-<<<<<<< HEAD
-    
-=======
 
 if __name__=="__main__":
     try:
         a = 1/0
     except Exception as e:
-        logging.info("Divide by zero error")
         raise CustomException(e, sys)
->>>>>>> cfa5f285d6d3b1352380e241d68ec5964121652c
